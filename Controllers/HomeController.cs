5f5ec4f55883d4469ca51cc8de2f4a34f815cff7
pndupont <pndupont@gmail.com>
2019-05-13 18:30:26 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DOJOSURVEY.Models;
+
+namespace DOJOSURVEY.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("result")]
+        public IActionResult Result(string Name, string Location, string FavStack, string Comment)
+        {
+            @ViewBag.Name = Name;
+            @ViewBag.Location = Location;
+            @ViewBag.FavStack = FavStack;
+            @ViewBag.Comment = Comment;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+    
